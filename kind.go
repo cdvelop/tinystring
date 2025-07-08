fb6e4d36a051fb48371eecf32dd7cb6b25b54fdb
César Solís <Csar Sols@users.noreply.github.com>
2025-07-08 18:35:25 -0400

    add kingstring in king

--- kind.go
+++ kind.go
@@ -35,3 +35,44 @@ const (
 	KUintptr
 	KUnsafePtr
 )
+
+// kindNames provides string representations for each kind value
+var kindNames = []string{
+	"array",
+	"bool",
+	"chan",
+	"complex128",
+	"complex64",
+	"err",
+	"float32",
+	"float64",
+	"func",
+	"int",
+	"int16",
+	"int32",
+	"int64",
+	"int8",
+	"interface",
+	"invalid",
+	"map",
+	"ptr",
+	"slice",
+	"string",
+	"[]string",
+	"struct",
+	"uint",
+	"uint16",
+	"uint32",
+	"uint64",
+	"uint8",
+	"uintptr",
+	"unsafe.Pointer",
+}
+
+// String returns the name of the kind as a string
+func (k kind) String() string {
+	if int(k) >= 0 && int(k) < len(kindNames) {
+		return kindNames[k]
+	}
+	return "invalid"
+}
